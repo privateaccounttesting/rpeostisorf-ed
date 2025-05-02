86cae760e0b2f05522a264e036bb8e3bb5e0eb4c
privateaccounttesting <freesunshinegroup@proton.me>
2025-05-02 11:17:41 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/nicossteam/piratelive/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.nicossteam.piratelive
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.leanback.app.BrowseSupportFragment
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_browse_fragment, MainBrowseFragment())
+                .commitNow()
+        }
+    }
+} 
